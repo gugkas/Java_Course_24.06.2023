29dd78474c4d0588c7cdbcd558305e212fd67454
gugkas <gugkas@yandex.ru>
2023-07-05 13:46:19 +0300

    task1_DZ4

--- DZ4/task1_DZ4.java
+++ DZ4/task1_DZ4.java
@@ -1,5 +1,26 @@
 package DZ4;
 
+import java.util.LinkedList;
+
 public class task1_DZ4 {
-    
+
+    public static void main(String[] args) {
+
+        int[] listMas = { 5, 3, 2, 6, 7, 4, 9, 1 };
+        LinkedList<Integer> linkList = new LinkedList<>();
+
+        for (Integer temp : listMas) {
+            linkList.add(temp);
+        }
+
+        int temp = 0;
+        while (temp != linkList.size() - 1) {
+            linkList.add(linkList.size() - temp, linkList.getFirst());
+            linkList.removeFirst();
+            temp++;
+        }
+
+        System.out.println("linkList = " + linkList);
+
+    }
 }
